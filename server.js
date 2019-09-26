62dfa7549f0df3533752b5bfd3287ce6bcab7765
Sandeep <Sandeep>
2019-09-26 19:38:59 +0530

    Adding Site Template Code

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+app.use(express.static('public'))
 app.get('/', (req, res) => res.sendFile(__dirname+'/public/views/index.html'))
+app.get('/generic', (req, res) => res.sendFile(__dirname+'/public/views/generic.html'))
+app.get('/elements', (req, res) => res.sendFile(__dirname+'/public/views/elements.html'))
+
 app.get('/welcome', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
